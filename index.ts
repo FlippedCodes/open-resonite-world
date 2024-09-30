ee2378baf02ff6f473dc09a049d8ede8ea8b6eee
Phil <philip.schaefer0@gmail.com>
2024-09-30 21:41:34 +0200

    upgraded to bun serve, to counter cors origin issues

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { serve } from "bun";
+
+if (process.env.NODE_ENV === "development")
+  console.debug(`🚧 Running in debug mode!`);
+
+serve({
+  port: process.env.PORT || 3000,
+  async fetch(req) {
+    // Extract sessionID from the current URL path
+    const url = new URL(req.url);
+    const sessionID = url.pathname.split("/")[1];
+    if (!sessionID) return new Response(`No sessionID provided! Example: "${`${url}`}S-U-h33tology:fitness"`, { status: 400 });
+    // send API request to Resonite to get lnl-net link
+    const sessionDetailsRaw = await fetch(`${process.env.RESONITE_API_ENDPOINT}/${sessionID}`);
+    const sessionDetails = await sessionDetailsRaw.json();
+
+    // verify the existence and redirect to steam
+    if (sessionDetails.status) return new Response(sessionDetails.title, { status: sessionDetails.status });
+    const lnl = sessionDetails.sessionURLs[0];
+    if (!lnl) return new Response(`Session "${sessionID}" doesn\'t have a lnl-net link.`, { status: 404 });
+    return Response.redirect(`steam://rungameid/2519830//-Open ${encodeURIComponent(lnl)}`);
+  },
+});
